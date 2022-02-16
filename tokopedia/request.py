e871d18fc837addad8777cc997917e60f23b3973
Bow Vernon <bowvernon110391@gmail.com>
2022-02-16 11:13:09 +0700

    add fn to request pdplayout and product detail

--- tokopedia/request.py
+++ tokopedia/request.py
@@ -1,7 +1,10 @@
+import json
 import os
 import requests
 from helpers.helpers import load_json
 from urllib.parse import parse_qsl, urlencode
+
+tokopedia_endpoint = 'https://gql.tokopedia.com/'
 
 def tokped_search_product(q, page=1, rows=60):
     """Cari produk di tokopedia
@@ -26,7 +29,7 @@ def tokped_search_product(q, page=1, rows=60):
 
     req_search_prod[0]['variables']['params'] = urlencode(parsed)
 
-    result = requests.post('https://gql.tokopedia.com/', json=req_search_prod)
+    result = requests.post(tokopedia_endpoint, json=req_search_prod)
 
     if result.status_code == 200:
         return result.json()
@@ -76,3 +79,72 @@ def tokped_get_product_key(prod):
 
     gaKey = prod['gaKey'] or ''
     return str.split(gaKey, '/')[-1]
+
+def tokped_request_pdpsession(shop_domain, prod_key):
+    '''request pdp session untuk request detail produk
+
+    parameter:
+
+    @shop_domain : domain toko
+    @prod_key : product key
+
+    return : json dari graphql request
+    '''
+
+    filename = os.path.join(os.path.dirname(__file__), 'json/req_prod_detail_layout.json')
+    req_prod_detail = load_json(filename)
+
+    # modify some variable
+    req_prod_detail[0]['variables']['shopDomain'] = shop_domain
+    req_prod_detail[0]['variables']['productKey'] = prod_key
+
+    # send request?
+    # do not forget, add { X-TKPD-Akamai : pdpGetLayout } to header
+    result = requests.post(tokopedia_endpoint, json=req_prod_detail, headers={
+        'X-TKPD-Akamai' : 'pdpGetLayout'
+    })
+
+    if result.status_code == 200:
+        return result.json()
+    else:
+        return None
+
+def tokped_get_pdpsession_string(res):
+    if not res:
+        return None
+
+    return res[0]['data']['pdpGetLayout']['pdpSession']
+
+def tokped_request_product_detail(pdplayout):
+    '''ambil detil produk.
+
+    parameter:
+
+    @pdplayout : hasil dari request pdpsession
+
+    return : json berisi detil dari produk
+    '''
+    if not pdplayout:
+        return None
+
+    # grab pdpSession string and item id as string
+    pdpsession = tokped_get_pdpsession_string(pdplayout)
+    prod_id_str = pdplayout[0]['data']['pdpGetLayout']['basicInfo']['id']
+
+    # load the json file
+    filename = os.path.join(os.path.dirname(__file__), 'json/req_get_prod_detail.json')
+    req_prod_detail = load_json(filename)
+
+    # inject our data
+    req_prod_detail[0]['variables']['productID'] = prod_id_str
+    req_prod_detail[0]['variables']['pdpSession'] = pdpsession
+
+    # send request (no special header required?)
+    result = requests.post(tokopedia_endpoint, json=req_prod_detail)
+
+    if result.status_code == 200:
+        return result.json()
+    else:
+        return None
+
+
