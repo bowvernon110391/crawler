2e5cee9e0d38d5f8d9126dccbabd31ee9862e79b
Bow Vernon <bowvernon110391@gmail.com>
2022-03-04 16:41:39 +0700

    add crawl_tokopedia.py root script. works nice, but untested

---
+++ crawl_tokopedia.py
@@ -0,0 +1,61 @@
+import argparse
+import sys
+from helpers.helpers import get_lock, release_lock
+from tokopedia.request import tokped_search_product, tokped_get_products_from_result
+from helpers.funcs import tokped_transform_product
+import json
+
+def main():
+    parser = argparse.ArgumentParser(description="Crawl a specific amount of item on a specific page given a keyword")
+
+    '''
+    Parameter definition
+    '''
+    # must be provided
+    parser.add_argument('keyword', type=str, help='Keyword to search')
+
+    # optional
+    parser.add_argument('-p', '--page', type=int, default=1,help="The page to visit. defaults to 1")
+    parser.add_argument('-n', '--number', type=int, default=60, help="The number of items to grab. defaults to 60")
+    parser.add_argument('-l', '--lock', type=int, default=0, help="The lock id for concurrency. defaults to 0")
+    parser.add_argument('--pretty', action='store_true', help='Whether to indent the result or not')
+
+    # parse args
+    args = parser.parse_args()
+
+    # attempt to lock
+    if not get_lock(args.lock):
+        print(f"Crawler #{args.lock} already running...")
+        sys.exit(2)
+
+    # could say we succeeds?
+    try:
+        # do the job
+        # search product
+        result = tokped_search_product(q=args.keyword, page=args.page, rows=args.number)
+
+        # parse product
+        prods = tokped_get_products_from_result(result)
+
+        # store + transform result
+        items = []
+        
+        for prod in prods:
+            item = tokped_transform_product(prod)
+            items.append(item)
+        
+        # return as json dump
+        print(json.dumps(items,indent=2 if args.pretty else None))
+    except Exception as ex:
+        """ template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message) """
+        print(ex.with_traceback())
+
+        sys.exit(1)
+    finally:
+        release_lock(args.lock)
+
+
+if __name__ == '__main__':
+    main()
--- helpers/funcs.py
+++ helpers/funcs.py
@@ -16,6 +16,8 @@ def gmap_reverse_geocode(coord):
         return locations[0]['formatted_address']
     return '-'
 
+def tokped_parse_tags(ts):
+    return list( map( lambda s: re.sub('[\-\.]', ' ', s), ts.split('/')) )
 
 def tokped_transform_product(prod):
     # first get detail
@@ -31,10 +33,19 @@ def tokped_transform_product(prod):
     last_active_timestamp = datetime.fromtimestamp(int(d['shopInfo']['shopLastActive'])).strftime('%Y-%m-%d %H:%M:%S')
     registered_at = datetime.fromtimestamp(int(d['shopInfo']['createInfo']['epochShopCreated'])).strftime('%Y-%m-%d %H:%M:%S')
 
+    # some data may be empty?
     if d['nearestWarehouse'] and len(d['nearestWarehouse']) > 0:
         kode_pos = d['nearestWarehouse'][0]['warehouse_info']['postal_code']
-        lat, lon = d['nearestWarehouse'][0]['warehouse_info']['geolocation'].split(',')
-    
+
+        # sometimes coordinate isn't provided
+        coord = d['nearestWarehouse'][0]['warehouse_info']['geolocation']
+
+        if len(coord) > 3:
+            (lat, lon) = coord.split(',')
+
+
+    """ if type(lat) == tuple:
+        print('cause: ' + json.dumps(d['nearestWarehouse'][0]['warehouse_info'], indent=2)) """
 
     item = {
         # item_id is unique per marketplace
@@ -42,12 +53,14 @@ def tokped_transform_product(prod):
         'item_id': prod['id'],
         'name': prod['name'],
         'rating': int(prod['rating']),
-        'rating_avg': float(prod['ratingAverage']),
+        'rating_avg': None if not prod['ratingAverage'] else float(prod['ratingAverage']),
         'price': float(re.sub('[^0-9]','', prod['price'])),
         'url': prod['url'],
         'image_url': prod['imageUrl'],
         'view_count': int(d['productView']),
         'wishlist_count': int(d['wishlistCount']),
+        'category_name': prod['categoryName'],
+        'tags': tokped_parse_tags(prod['categoryBreadcrumb']),
         # shop data
         'shop': {
             'id': prod['shop']['shopId'],
@@ -55,11 +68,11 @@ def tokped_transform_product(prod):
             'url': prod['shop']['url'],
             'domain': d['shopInfo']['shopCore']['domain'],
             'kota': prod['shop']['city'],
-            'alamat': gmap_reverse_geocode((lat, lon)),
+            'alamat': '-' if not (lat and lon) else gmap_reverse_geocode((lat, lon)),
             'last_active_timestamp': last_active_timestamp,
             'kode_pos': kode_pos,
-            'lat': float(lat),
-            'lon': float(lon),
+            'lat': float(lat) if lat and lon else None,
+            'lon': float(lon) if lon and lat else None,
             'registered_at': registered_at,
             # our custom marker
             'marketplace': 'tokopedia'
